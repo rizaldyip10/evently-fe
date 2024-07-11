5ca8e96cc89521591d4ba1f2140e29c5386f2dab
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-07-11 20:15:27 +0700

    updating dependencies after merging

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -7,8 +7,8 @@ import { Button } from '@/components/ui/button';
 import loginasset from "@/assets/image/login-assets.svg";
 import singupassetweb from "@/assets/image/signup-assets-web.svg";
 import { Form, Formik } from "formik";
-import InputField from '@/components/page/global/inputfield';
 import RegisterDialog from '@/components/page/global/RegisterDialog';
+import InputField from '@/components/pages/global/inputfield';
 
 const RegisterPage = () => {
 
